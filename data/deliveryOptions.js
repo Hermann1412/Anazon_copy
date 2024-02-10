28ae67b66ee0c18434b5ddb4fdd37ea4def9173f
Hermann1412 <u6411076@au.edu>
2024-02-11 03:46:02 +0700

    Save data and generate HTML for delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,15 @@
+//we create the list of delivery option
+
+export const deliveryOptions = [{
+  id: '1',
+  deliveryDays: 7,
+  priceCents: 0
+}, {
+  id: '2',
+  deliveryDays: 3,
+  priceCents: 499
+}, {
+  id: '3',
+  deliveryDays: 1,
+  priceCents: 999
+}];
