189ad962451a1c18a39768a84cf4fd0054e943a6
Hermann1412 <u6411076@au.edu>
2024-02-12 00:52:15 +0700

    Save data and generate HTML for payment summary

--- data/products.js
+++ data/products.js
@@ -1,4 +1,15 @@
 //this file create all the product
+export function getProduct(productId) {
+  let matchingProduct;
+
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+
+  return matchingProduct;
+}
 
 export const products = [
   {
